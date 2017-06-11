4a32a21789a5fe884bc119d6f003acc0207b3920
bcaneba <bcaneba@gmail.com>
2017-06-11 11:37:29 -0500

    added migrations for backend

--- db/migrate/20170610134328_create_game.rb
+++ db/migrate/20170610134328_create_game.rb
@@ -1,8 +1,7 @@
 class CreateGame < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-    	
-    	t.boolean, :x_is_next, default: true
+    	t.string :name
     end
   end
 end
